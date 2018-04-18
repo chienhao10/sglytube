37637f4a6abca9f72458c1cbd8d0df78be49997d
chienhao10 <chienhao10@users.noreply.github.com>
2018-04-18 15:00:08 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define('ROOT_PART', Root_part());
+define('APIKEY', getenv("AIzaSyDBc4LDa3V5rC5kNDcZgmQ6jrcCbxZ4D14"));
+define('GJ_CODE', getenv("TW"));
+define('SITE_NAME',getenv("SGLY Tube"));
+define('TITLENAME', getenv("SGLY Tube"));
+define('EN2DEKEY',getenv("encryptkey"));
+define('EMAIL', getenv("@justin10"));
+?>
